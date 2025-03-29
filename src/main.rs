0374ff858ce339c1dee41f909c922855dba020c2
Nemin32 <pergerlori@gmail.com>
2025-03-29 14:49:56 +0100

    Use threads

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{fs, ops::Neg};
+use std::{fs, ops::Neg, sync::{Arc, RwLock}, thread};
 
-const DELTA: f64 = 1.0 / 100.0;
+const RESOLUTION: f64 = 100.0;
+const DELTA: f64 = 1.0 / RESOLUTION;
 
 #[derive(Debug, Clone, Copy)]
 enum OpCode {
@@ -33,14 +34,11 @@ impl From<&str> for OpCode {
         let arg1u = arg1
             .as_ref()
             .and_then(|inner| usize::from_str_radix(inner, 16).ok());
-        let arg1f = arg1
-            .as_ref()
-            .and_then(|inner| inner.parse::<f64>().ok());
+        let arg1f = arg1.as_ref().and_then(|inner| inner.parse::<f64>().ok());
 
-        let arg2 = parts
-            .get(3)
-            .map(|e| e.trim_start_matches("_"))
-            .map(|e| usize::from_str_radix(e, 16).unwrap_or_else(|_| panic!("Failed to parse: {}", e)));
+        let arg2 = parts.get(3).map(|e| e.trim_start_matches("_")).map(|e| {
+            usize::from_str_radix(e, 16).unwrap_or_else(|_| panic!("Failed to parse: {}", e))
+        });
 
         match code {
             "var-y" => VarY,
@@ -110,23 +108,33 @@ fn main() {
     }
 
     let mut y: f64 = -1.0;
-    let mut x: f64 = -1.0;
+
+    let mut threads = Vec::new();
+    let shared_ops: Arc<RwLock<Vec<OpCode>>> = Arc::new(RwLock::new(opcodes));
 
     while y <= 1.0 {
-        while x <= 1.0 {
-            let value = interpret(&opcodes, x, -y);
+        let opcodes = shared_ops.clone();
+        threads.push(thread::spawn(move || {
+            let mut row: Vec<bool> = Vec::with_capacity(usize::from(RESOLUTION as u16) * 2);
+            let mut x: f64 = -1.0;
 
-            if value.is_sign_positive() {
-                print!(".");
-            } else {
-                print!("#");
+            while x <= 1.0 {
+                let value = interpret(&opcodes.read().unwrap(), x, -y);
+                row.push(value.is_sign_positive());
+                x += DELTA;
             }
 
-            x += DELTA;
-        }
+            ((y * RESOLUTION) as i64, row)
+        }));
 
-        println!();
         y += DELTA;
-        x = -1.0;
+    }
+
+    let mut final_rows = threads.into_iter().map(|r| r.join().unwrap()).collect::<Vec<(i64, Vec<bool>)>>();
+    final_rows.sort_by_key(|(i, _)| *i);
+
+    for (_, row) in final_rows {
+        let line = row.iter().map(|e| if *e {"."} else {"#"}).collect::<String>();
+        println!("{}", line);
     }
 }
