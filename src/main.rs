4db753557aa622faf33de81c44873feed3f7b822
Nemin32 <pergerlori@gmail.com>
2025-03-29 15:51:11 +0100

    Reorganize lines

--- src/main.rs
+++ src/main.rs
@@ -57,9 +57,7 @@ impl From<&str> for OpCode {
             "sqrt" => Sqrt(args[0]),
             "min" => Min(args[0], args[1]),
             "max" => Max(args[0], args[1]),
-            _ => {
-                panic!("Unexpected code: {}", code)
-            }
+            _ => unreachable!("Unexpected code: {}", code),
         }
     }
 }
@@ -92,25 +90,18 @@ fn interpret(opcodes: &[OpCode], x: f64, y: f64) -> f64 {
 
 fn main() {
     let file = fs::read_to_string("./prospero.vm").expect("File to be present.");
-
     let opcodes: Vec<OpCode> = file.lines().map(|e| e.into()).collect();
-
-    for opcode in &opcodes {
-        println!("{:?}", opcode);
-    }
+    let shared_ops: Arc<RwLock<Vec<OpCode>>> = Arc::new(RwLock::new(opcodes));
+    let mut threads = Vec::new();
 
     let mut y: f64 = -1.0;
-
-    let mut threads = Vec::new();
-    let shared_ops: Arc<RwLock<Vec<OpCode>>> = Arc::new(RwLock::new(opcodes));
-
     while y <= 1.0 {
         let opcodes = shared_ops.clone();
         threads.push(thread::spawn(move || {
             let mut row: Vec<bool> = Vec::with_capacity(usize::from(RESOLUTION) * 2);
-            let mut x: f64 = -1.0;
             let opcodes = opcodes.read().unwrap();
 
+            let mut x: f64 = -1.0;
             while x <= 1.0 {
                 let value = interpret(&opcodes, x, -y);
                 row.push(value.is_sign_positive());
