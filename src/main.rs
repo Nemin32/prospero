d7ebcff9bc609745771490a7b08f21410f93471f
Nemin32 <pergerlori@gmail.com>
2025-03-29 19:39:25 +0100

    Basic optimization

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
 
 use rayon::prelude::*;
 
-const RESOLUTION: u16 = 1024;
+const RESOLUTION: u16 = 128;
 const DELTA: f64 = 1.0 / (RESOLUTION as f64);
 
 #[derive(Debug, Clone, Copy)]
@@ -27,19 +27,34 @@ enum OpCode {
     // Compare
     Max(usize, usize),
     Min(usize, usize),
+    // Extend
+    ConstAdd(usize, f64),
+    ConstSub(usize, f64),
+    SubConst(f64, usize),
+    ConstMul(usize, f64),
 }
 
-impl From<&str> for OpCode {
+#[derive(Clone, Copy)]
+struct Instruction {
+    out: usize,
+    op: OpCode,
+}
+
+impl From<&str> for Instruction {
     fn from(value: &str) -> Self {
         use OpCode::*;
 
         let parts = value.split(" ").collect::<Vec<_>>();
+        let out = usize::from_str_radix(parts[0].trim_start_matches("_"), 16).unwrap();
         let code = parts[1];
         let args = &parts[2..];
 
         if code == "const" {
             let arg = args[0].parse::<f64>().unwrap();
-            return Const(arg);
+            return Instruction {
+                out,
+                op: Const(arg),
+            };
         }
 
         let args = args
@@ -47,7 +62,7 @@ impl From<&str> for OpCode {
             .map(|e| usize::from_str_radix(e.trim_start_matches("_"), 16).unwrap())
             .collect::<Vec<usize>>();
 
-        match code {
+        let op = match code {
             "var-y" => VarY,
             "var-x" => VarX,
             "add" => Add(args[0], args[1]),
@@ -59,17 +74,92 @@ impl From<&str> for OpCode {
             "min" => Min(args[0], args[1]),
             "max" => Max(args[0], args[1]),
             _ => unreachable!("Unexpected code: {}", code),
-        }
+        };
+
+        Instruction { out, op }
     }
 }
 
-fn interpret(opcodes: &[OpCode], x: f64, y: f64) -> f64 {
+fn optimize(instructions: &[Instruction]) -> Vec<Instruction> {
     use OpCode::*;
 
-    let mut map: Vec<f64> = Vec::with_capacity(opcodes.len());
+    let mut optimized: Vec<Instruction> = Vec::new();
 
-    for opcode in opcodes {
-        let value = match *opcode {
+    for window in instructions.chunks(2) {
+        let value = match window {
+            [
+                Instruction {
+                    out: out1,
+                    op: Const(c),
+                },
+                Instruction {
+                    out: out2,
+                    op: Add(addr1, addr2),
+                },
+            ] if out1 == addr1 || out1 == addr2 => vec![
+                Instruction {out: *out1, op: Const(*c)},
+                Instruction {
+                out: *out2,
+                op: if out1 == addr1 {
+                    ConstAdd(*addr2, *c)
+                } else {
+                    ConstAdd(*addr1, *c)
+                },
+            }],
+            [
+                Instruction {
+                    out: out1,
+                    op: Const(c),
+                },
+                Instruction {
+                    out: out2,
+                    op: Sub(addr1, addr2),
+                },
+            ] if out1 == addr1 || out1 == addr2 => vec![
+                Instruction {out: *out1, op: Const(*c)},
+                Instruction {
+                out: *out2,
+                op: if out1 == addr1 {
+                    SubConst(*c, *addr2)
+                } else {
+                    ConstSub(*addr1, *c)
+                },
+            }],
+            [
+                Instruction {
+                    out: out1,
+                    op: Const(c),
+                },
+                Instruction {
+                    out: out2,
+                    op: Mul(addr1, addr2),
+                },
+            ] if out1 == addr1 || out1 == addr2 => vec![
+                Instruction {out: *out1, op: Const(*c)},
+                Instruction {
+                out: *out2,
+                op: if out1 == addr1 {
+                    ConstMul(*addr2, *c)
+                } else {
+                    ConstMul(*addr1, *c)
+                },
+            }],
+            _ => window.to_vec(),
+        };
+
+        optimized.extend(value);
+    }
+
+    optimized
+}
+
+fn interpret(instructions: &[Instruction], x: f64, y: f64) -> f64 {
+    use OpCode::*;
+
+    let mut map: Vec<f64> = Vec::with_capacity(instructions.len());
+
+    for (i, Instruction { out: _, op }) in instructions.iter().enumerate() {
+        let value = match *op {
             VarY => y,
             VarX => x,
             Add(k1, k2) => map[k1] + map[k2],
@@ -81,12 +171,16 @@ fn interpret(opcodes: &[OpCode], x: f64, y: f64) -> f64 {
             Sqrt(k) => map[k].sqrt(),
             Max(k1, k2) => map[k1].max(map[k2]),
             Min(k1, k2) => map[k1].min(map[k2]),
+            ConstAdd(k, v) => map[k] + v,
+            ConstSub(k, v) => map[k] - v,
+            SubConst(v, k) => v - map[k],
+            ConstMul(k, v) => map[k] * v,
         };
 
-        map.push(value);
+        map.insert(i, value);
     }
 
-    map[opcodes.len() - 1]
+    map[instructions.len() - 1]
 }
 
 fn main() {
@@ -94,8 +188,16 @@ fn main() {
     let file = fs::read_to_string("./prospero.vm").expect("File to be present.");
 
     // Parse opcodes
-    let opcodes: Vec<OpCode> = file.par_lines().map(|e| e.into()).collect();
-    let shared_ops: Arc<RwLock<Vec<OpCode>>> = Arc::new(RwLock::new(opcodes));
+    let opcodes: Vec<Instruction> = file.par_lines().map(|e| e.into()).collect();
+    let opcodes = optimize(&opcodes);
+
+    for op in &opcodes {
+        println!("{} - {:?}", op.out, op.op);
+    }
+
+
+    let shared_ops: Arc<RwLock<Vec<Instruction>>> = Arc::new(RwLock::new(opcodes));
+
 
     // Precompute matrix
     let mut values = Vec::new();
